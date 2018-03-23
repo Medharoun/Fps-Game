1811c52d2de555faede92e0a4a4e6a11e9e21ab3
Medharoun <m.haroun.neffati@gmail.com>
2018-03-23 01:55:57 +0100

    23/03 01:55

--- Fps Game/Assets/PlayerController.cs
+++ Fps Game/Assets/PlayerController.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent (typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private float speed = 5f;
+
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update ()
+	{
+		//calcul movemement velocity vector3
+		float xMov = Input.GetAxisRaw ("Horizontal");
+		float zMov = Input.GetAxisRaw ("Vertical");
+
+		Vector3 movHorizontal = transform.right * xMov;
+		Vector3 movVertical = transform.forward * zMov;
+
+		Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+		motor.Move (velocity);
+
+		//calcul rotation  vector3 (turning around)
+		float yRot = Input.GetAxisRaw ("Mouse X");
+
+		Vector3 rotation = new Vector3 (0, yRot, 0) * lookSensitivity;
+
+		motor.Rotation (rotation);
+
+
+		float xRot = Input.GetAxisRaw ("Mouse Y");
+
+		Vector3 cameraRotation = new Vector3 (xRot, 0, 0) * lookSensitivity;
+
+		motor.rotateCamera (cameraRotation);
 	}
 }
--- Fps Game/Assets/PlayerMotor.cs
+++ Fps Game/Assets/PlayerMotor.cs
@@ -1,16 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMotor : MonoBehaviour {
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private Camera cam;
+
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 cameraRotation = Vector3.zero;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void Move (Vector3 _velocity)
+	{
+		velocity = _velocity;
 	}
+
+	void FixedUpdate ()
+	{
+		PerformMovement ();
+		PerformRotation ();
+	}
+
+	void PerformMovement ()
+	{
+		if (velocity != Vector3.zero) {
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	void PerformRotation ()
+	{		
+		rb.MoveRotation (rb.rotation * Quaternion.Euler (rotation));
+		if (cam != null) 
+		{
+			cam.transform.Rotate(-cameraRotation);
+		}
+	}
+
+	public void Rotation (Vector3 _rotation)
+	{
+		rotation = _rotation;
+	}
+
+
+	public void rotateCamera (Vector3 _cameraRotation)
+	{
+		cameraRotation = _cameraRotation;
+	}
+
 }
